6463634c3f9127a8a8c2ee3b1867fa02b6986835
Christopher Mann <christopher.mann@dbosoft.eu>
2025-11-12 11:17:02 +0100

    Continue

---
+++ src/OVN.Primitives/Model/OVN/SouthboundGlobal.cs
@@ -0,0 +1,21 @@
+﻿using LanguageExt;
+
+namespace Dbosoft.OVN.Model.OVN;
+
+public record SouthboundGlobal : OVSTableRecord, IOVSEntityWithName, IHasOVSReferences<SouthboundConnection>
+{
+    public new static readonly IDictionary<string, OVSFieldMetadata>
+        Columns = new Dictionary<string, OVSFieldMetadata>(OVSTableRecord.Columns)
+        {
+            { "connections", OVSReference.Metadata() },
+            //{ "ssl", OVSReference.Metadata() }
+        };
+
+    public Seq<Guid> Connections => GetReference("connections");
+
+    //public Seq<Guid> Ssl => GetReference("ssl");
+
+    public string? Name => ".";
+
+    Seq<Guid> IHasOVSReferences<SouthboundConnection>.GetOvsReferences() => Connections;
+}
