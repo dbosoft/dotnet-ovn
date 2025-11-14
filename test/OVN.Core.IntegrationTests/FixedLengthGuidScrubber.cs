593ae4f5f52ed7eb83a6ebb880be11bb6ea60ebd
Christopher Mann <christopher.mann@dbosoft.eu>
2025-11-14 13:10:15 +0100

    Cleanup GUID scrubber

--- test/OVN.Core.IntegrationTests/FixedLengthGuidScrubber.cs
+++ test/OVN.Core.IntegrationTests/FixedLengthGuidScrubber.cs
@@ -10,45 +10,32 @@ public static class FixedLengthGuidScrubber
     public static void ReplaceGuids(StringBuilder builder, Counter counter)
     {
         if (!counter.ScrubGuids)
-        {
             return;
-        }
-
-        //{173535ae-995b-4cc6-a74e-8cd4be57039c}
+        
         if (builder.Length < 36)
-        {
             return;
-        }
-
+        
         var value = builder.ToString().AsSpan();
-
-        var builderIndex = 0;
+        
         for (var index = 0; index <= value.Length; index++)
         {
             var end = index + 36;
             if (end > value.Length)
-            {
                 return;
-            }
 
-            if ((index == 0 || !IsInvalidStartingChar(value[index - 1])) &&
-                (end == value.Length || !IsInvalidEndingChar(value[end])))
+            if (index != 0 && IsInvalidStartingChar(value[index - 1])
+                || end != value.Length && IsInvalidEndingChar(value[end]))
             {
-                var slice = value.Slice(index, 36);
-                if (slice.IndexOfAny('\r', '\n') == -1 &&
-                    Guid.TryParseExact(slice, "D", out var guid))
-                {
-                    var convert = counter.Next(guid);
-                    builder.Remove(index, 36);
-                    builder.Insert(index, $"Guid_{convert:0000000000000000000000000000000}");
-                    builderIndex += 36;
-                    index += 35;
-
-                    continue;
-                }
+                continue;
             }
-
-            builderIndex++;
+            var slice = value.Slice(index, 36);
+            if (slice.IndexOfAny('\r', '\n') != -1 || !Guid.TryParseExact(slice, "D", out var guid))
+                continue;
+            
+            var convert = counter.Next(guid);
+            builder.Remove(index, 36);
+            builder.Insert(index, $"Guid_{convert:0000000000000000000000000000000}");
+            index += 35;
         }
     }
 
